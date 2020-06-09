80518b6343beb14795972ebd4f5d4f0bee0b7a61
Nauman <nauman.mir@gmail.com>
2020-06-09 22:53:01 +0200

    Added table - cut 1

--- src/components/members/MemberSignUps.jsx
+++ src/components/members/MemberSignUps.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import '../../App.css';
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import './MemberSignUps.css';
+import DataTable from '../table/DataTable';
 
 function MemberSignUps(props) {
     const [startDate, setStartDate] = useState(new Date());
@@ -11,18 +12,53 @@ function MemberSignUps(props) {
 
     useEffect(() => {fetchMembersData()}, [fetchMembersData]);
 
+    const data = [
+        {
+            col1: 'Hello',
+            col2: 'World',
+        },
+        {
+            col1: 'react-table',
+            col2: 'rocks',
+        },
+        {
+            col1: 'whatever',
+            col2: 'you want',
+        },
+    ];
+
+    const cols = [
+        {
+            Header: 'Column 1',
+            accessor: 'col1', // accessor is the "key" in the data
+            filter: 'fuzzyText',
+        },
+        {
+            Header: 'Column 2',
+            accessor: 'col2',
+            filter: 'fuzzyText',
+        },
+    ];
+
     return (
         <div className='AppMainSection'>
-            From
-            <DatePicker
-                selected={startDate}
-                onChange={(newStartDate) => setStartDate(newStartDate)} />
-
-            To
-            <DatePicker
-                selected={endDate}
-                onChange={(newEndDate) => setEndDate(newEndDate)} />
-
+            <h2>
+                Specify Sign-Up date range
+            </h2>
+            <div className='MemberSignUpDateSection'>
+                <div>From</div>
+                <DatePicker
+                    selected={startDate}
+                    onChange={(newStartDate) => setStartDate(newStartDate)} />
+                <div>To</div>
+                <DatePicker
+                    selected={endDate}
+                    onChange={(newEndDate) => setEndDate(newEndDate)} />
+                <button>Fetch</button>
+            </div>
+            <div className='MemberSignUpDateSection'>
+                <DataTable tableHeaders={cols} tableData={data} />
+            </div>
         </div>
     );
 }
---
+++ src/components/table/DataTable.jsx
@@ -0,0 +1,168 @@
+import React, { useEffect, useState } from 'react';
+import {
+    useTable,
+    useFilters,
+    useGlobalFilter,
+    useAsyncDebounce,
+    useSortBy,
+    useExpanded,
+    usePagination,
+} from 'react-table';
+
+import matchSorter from 'match-sorter';
+import './DataTable.css';
+
+// Define a default UI for filtering
+function GlobalFilter({
+                          preGlobalFilteredRows,
+                          globalFilter,
+                          setGlobalFilter,
+                      }) {
+    const count = preGlobalFilteredRows.length
+    const [value, setValue] = React.useState(globalFilter)
+    const onChange = useAsyncDebounce(value => {
+        setGlobalFilter(value || undefined)
+    }, 200)
+
+    return (
+        <span>
+      Search:{' '}
+            <input
+                value={value || ""}
+                onChange={e => {
+                    setValue(e.target.value);
+                    onChange(e.target.value);
+                }}
+                placeholder={`${count} records...`}
+                style={{
+                    fontSize: '1.1rem',
+                    border: '0',
+                }}
+            />
+    </span>
+    )
+}
+
+// Define a default UI for filtering
+function DefaultColumnFilter(
+    {
+      column: { filterValue, preFilteredRows, setFilter },
+    }) {
+    const count = preFilteredRows.length
+
+    return (
+        <input
+            value={filterValue || ''}
+            onChange={e => {
+                setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+            }}
+            placeholder={`Search ${count} records...`}
+        />
+    )
+}
+
+function fuzzyTextFilterFn(rows, id, filterValue) {
+    return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = val => !val
+
+function DataTable(props) {
+    const filterTypes = React.useMemo(
+        () => ({
+            // Add a new fuzzyTextFilterFn filter type.
+            fuzzyText: fuzzyTextFilterFn,
+            // Or, override the default text filter to use
+            // "startWith"
+            text: (rows, id, filterValue) => {
+                return rows.filter(row => {
+                    const rowValue = row.values[id]
+                    return rowValue !== undefined
+                        ? String(rowValue)
+                            .toLowerCase()
+                            .startsWith(String(filterValue).toLowerCase())
+                        : true
+                })
+            },
+        }),
+        []
+    );
+
+    const defaultColumn = React.useMemo(
+        () => ({
+            // Let's set up our default Filter UI
+            Filter: DefaultColumnFilter,
+        }),
+        []
+    );
+
+    const { tableHeaders, tableData } = props;
+    const columns = React.useMemo(() => tableHeaders, []);
+    const data = React.useMemo(() => tableData, []);
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+        state,
+        visibleColumns,
+        preGlobalFilteredRows,
+        setGlobalFilter,
+    } = useTable({ columns, data, defaultColumn, filterTypes }, useFilters, useGlobalFilter, useSortBy);
+
+
+    return (
+        <div className='DataTable'>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                    {column.render('Header')}
+                                    <span>
+                                        {column.isSorted ?
+                                            column.isSortedDesc ? ' 🔽' : ' 🔼'
+                                            : ''}
+                                    </span>
+                                    <div>{column.canFilter ? column.render('Filter') : null}</div>
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                    <tr>
+                        <th
+                            colSpan={visibleColumns.length}
+                            style={{
+                                textAlign: 'left',
+                            }}
+                        >
+                            <GlobalFilter
+                                preGlobalFilteredRows={preGlobalFilteredRows}
+                                globalFilter={state.globalFilter}
+                                setGlobalFilter={setGlobalFilter}
+                            />
+                        </th>
+                    </tr>
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                {rows.map(row => {
+                    prepareRow(row)
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map(cell => {
+                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                            })}
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
