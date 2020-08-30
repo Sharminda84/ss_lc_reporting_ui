762d67c0699a5f12388e1dc55ba100c5b11628b0
Nauman <nauman.mir@gmail.com>
2020-08-30 18:04:06 +0100

    Added reporting

--- src/store/reducers/reports.js
+++ src/store/reducers/reports.js
@@ -6,8 +6,8 @@ import {
 const initialState = {
     configuredReports: [
         {
-            reportName: 'customer-orders.csv',
-            reportTitle: 'Customer Order Details',
+            reportName: 'orders.csv',
+            reportTitle: 'Orders',
             reportDescription: 'Details about customer orders. Report fields: Member ID, Card name, ' +
                 'Member create/update time, Card creation time, Card delivery date (deadline), ' +
                 'No of messages, Order ID, Transaction time, Customer email, Delivery address, Total cards ordered',
@@ -15,8 +15,8 @@ const initialState = {
             reportGenerator: customerOrdersReportCSVGenerator,
         },
         {
-            reportName: 'repeat-customers.csv',
-            reportTitle: 'Repeat Customers',
+            reportName: 'customer-orders-count.csv',
+            reportTitle: 'Customer Orders Count',
             reportDescription: 'Gives total cards bought by each member. Report fields: Customer ID, First Name, Email, No of cards ordered.',
             reportURL: '/reporting/analytics/order-count-per-customer',
             reportGenerator: repeatCustomersReportCSVGenerator,
