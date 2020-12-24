2f87b860921e5bb4c6a36468f31fa221cecf1e07
Nauman <nauman.mir@gmail.com>
2020-12-24 14:12:25 +0000

    Fixed the login dialog.

--- src/components/security/Login.jsx
+++ src/components/security/Login.jsx
@@ -1,45 +1,36 @@
 import React, { useState } from 'react';
-import Modal from 'react-modal';
+import { Redirect } from 'react-router-dom';
 import './Login.css';
-
-Modal.setAppElement(document.getElementById('root'));
 
 function Login(props) {
     const { loggedIn, logInError, triggerLogin } = props;
     const [user, setUser] = useState('');
     const [password, setPassword] = useState('');
 
-    const dialogStyle = {
-        content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)'
-        }
-    };
+    if (loggedIn) {
+        return <Redirect to={'/'} />
+    }
 
-  return (
-      <div>
-          <Modal
-              className='login'
-              isOpen={!loggedIn}
-              contentLabel='Log Innn'
-              style={dialogStyle}
-          >
-              <label className='title'>LOG IN</label>
-              <label className='user'>User</label>
-              <input className='userInput' type='text' name='user' onChange={e => setUser(e.target.value)} />
-              <label className='password'>Password</label>
-              <input className='passwordInput' type='password' name='password' onChange={e => setPassword(e.target.value)} />
-              {
-                  logInError && <label className='loginErrorMessage'>Error logging in. Please try again...</label>
-              }
-              <button className='loginButton' onClick={() => triggerLogin(user, password)}>Log In</button>
-          </Modal>
-      </div>
-  );
+
+    return (
+        !loggedIn &&
+        <div className='LoginPanel'>
+            <div className='LoginPanelTitle'>
+                LOG IN
+            </div>
+            <div className='LoginPanelError'>
+                {
+                    logInError &&
+                    <div>
+                        Error logging in. Please make sure that email and password are correct.
+                    </div>
+                }
+            </div>
+            <input className='LoginPanelInput' type='text' placeholder='Email...' onChange={e => setUser(e.target.value)} />
+            <input className='LoginPanelInput' type='password' placeholder='Password...' onChange={e => setPassword(e.target.value)} />
+            <button className='LoginPanelButton' onClick={() => triggerLogin(user, password)}>Log In</button>
+        </div>
+    );
 };
 
 export default Login;
