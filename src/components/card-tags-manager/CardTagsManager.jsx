5c61dd83b7093e4d558e3a2ca965412be1cb91ab
Nauman <nauman.mir@gmail.com>
2021-07-04 09:31:37 +0100

    Tag work - 4.

--- src/components/card-tags-manager/CardTagsManager.jsx
+++ src/components/card-tags-manager/CardTagsManager.jsx
@@ -3,42 +3,57 @@ import '../../App.css';
 import '../../components/orders/Orders.css';
 import './CardTagsManager.css';
 
+
+// tags.filter(c => c.tag === tag)[0].cardLinks
+
+
 const CardTagsManager = ( props ) => {
-    const { tags } = props;
-    const { createTag, updateTag, deleteTag } = props;
+    const {tags} = props;
+    const {createTag, updateTag, deleteTag} = props;
     const [tagSearchText, setTagSearchText] = useState('');
-    const [tagCreateText, setTagCreateText] = useState('');
-    const [currentTag, setcurrentTag] = useState('');
+    const [tagUpdateText, setTagUpdateText] = useState('');
 
-    const tagButtonClickHandler = (tag) => {
-        if (currentTag === tag) {
-            setcurrentTag('');
+    const [currentTag, setCurrentTag] = useState(null);
+    const [currentTagText, setcurrentTagText] = useState('');
+    const [currentTagLinksText, setCurrentTagLinksText] = useState('');
+
+    const tagButtonClickHandler = (tagText) => {
+        if (currentTagText === tagText) {
+            setcurrentTagText('');
+            setCurrentTag(null);
         } else {
-            setcurrentTag(tag);
-            // TODO display the tag details here in the tag details panel....
-
+            setcurrentTagText(tagText);
+            const currentTag = tags.filter(c => c.tag === tagText)[0];
+            setCurrentTag(currentTag);
+            setTagUpdateText(tagText);
+            const tagLinksText = currentTag
+                .cardLinks
+                .sort((link1, link2) => link1.tagDisplaySeq > link2.tagDisplaySeq ? 1 : -1)
+                .reduce((aggregate, current) => aggregate + current.cardDesignName + '\n', '');
+            setCurrentTagLinksText(tagLinksText);
         }
     }
 
     const deleteTagClickHandler = () => {
-        if (currentTag === '') {
+        if (currentTagText === '') {
             return;
         }
 
-        deleteTag(currentTag);
-        setcurrentTag('');
+        deleteTag(currentTagText);
+        setcurrentTagText('');
+        setCurrentTag(null);
     };
 
     const addTagClickHandler = () => {
-        if (tagCreateText === '') {
+        if (tagSearchText === '') {
             return;
         }
 
-        createTag(tagCreateText);
+        createTag(tagSearchText);
     }
 
     const renderTagButton = (index, tag) => {
-        const cssClass = tag === currentTag ? 'tagButton tagButtonSelected' : 'tagButton'
+        const cssClass = tag === currentTagText ? 'tagButton tagButtonSelected' : 'tagButton'
 
         return (
             <button key={index} className={cssClass} onClick={e => tagButtonClickHandler(tag)}>{tag}</button>
@@ -65,19 +80,27 @@ const CardTagsManager = ( props ) => {
                 {renderMatchingTags()}
             </div>
             <div className='tagSearchPanel'>
-                <input type='text' className='tagAddInput' placeholder='Create Tag...' onChange={e => setTagCreateText(e.target.value)} maxLength={200} />
+                <input type='text' className='tagAddInput' placeholder='Create Tag...' onChange={e => setTagUpdateText(e.target.value)} maxLength={200} />
                 <button className='addButton' onClick={addTagClickHandler}>+</button>
             </div>
             <div className='tagEditPanel'>
-                <div className='tagEditPanelDiv'>
+                <div>
                     <h5>Tag Name</h5>
-                    <input type='text' className='tagAddInput' placeholder='Create Tag...' onChange={e => setTagCreateText(e.target.value)} maxLength={200} />
+                    <input type='text'
+                           className='tagAddInput'
+                           placeholder='Create Tag...'
+                           value={tagUpdateText}
+                           onChange={e => setTagUpdateText(e.target.value)}
+                           maxLength={200} />
                 </div>
-                <div className='tagEditPanelDiv'>
+                <div>
                     <h5>Tag Links</h5>
-                    <textarea className='tagLinksTextArea' placeholder='Message...'  />
+                    <textarea className='tagLinksTextArea'
+                              placeholder='Tag links...'
+                              value={currentTagLinksText}
+                              onChange={e => setCurrentTagLinksText(e.target.value)} />
                 </div>
-                <button className='saveButton'>Save</button>
+                <button>Save</button>
             </div>
         </div>
     )
