033aa852ea5d3d1ec4c32a6caf2ca9c9b48e34ed
Nauman <nauman.mir@gmail.com>
2021-11-25 10:23:29 +0000

    Ad analytics - v1

--- src/components/ad-analytics/AdMetrics.jsx
+++ src/components/ad-analytics/AdMetrics.jsx
@@ -5,9 +5,49 @@ function AdMetrics(props) {
 
     const { adMetrics } = props;
 
+    const printMetric = (adMetric) => {
+
+        return (
+            <div className='AdMetric'>
+                <div className='AdMetricRow HeaderRow'>
+                    <div>Ad: {adMetric.adGroupId}</div>
+                    <div>Account: {adMetric.accountId}</div>
+                    <div>Campaign: {adMetric.campaignId}</div>
+                </div>
+                <div className='AdMetricRow'>
+                    Conversion Ratio: {adMetric.convertedToLandedRatio}
+                </div>
+                <div className='AdMetricRow'>
+                    Landed count: {adMetric.landedCount}
+                </div>
+                <div className='AdMetricRow'>
+                    Total registered: {adMetric.registeredCount}
+                </div>
+                <div className='AdMetricRow'>
+                    Total eCards Purchased: {adMetric.ecardPurchasedCount}
+                </div>
+                <div className='AdMetricRow'>
+                    Total A4 purchased: {adMetric.a4PurchasedCount}
+                </div>
+                <div className='AdMetricRow'>
+                    Total A5 purchased: {adMetric.a5PurchasedCount}
+                </div>
+                <div className='AdMetricRow'>
+                    Key Words: {adMetric.keyWords.map(keyWord => "[" + keyWord + "]  ")}
+                </div>
+            </div>
+        );
+
+    };
+
     return (
-        <div className=''>
-            Ad Metrics Length: {adMetrics.length}
+        <div className='AdMetrics'>
+            <div className='AdMetricsTitle'>
+                Ad Metrics
+            </div>
+            {
+                adMetrics.map(adMetric => printMetric(adMetric))
+            }
         </div>
     );
 }
